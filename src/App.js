151b2ecd2cdb7470fee4f2f0a226d1c53a1dc084
MuhammadHaris2 <mhari8339@gmail.com>
2021-05-16 06:50:58 +0500

    comment app

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
   const [mapZoom, setMapZoom] = useState(3);
   const [mapCountries, setMapCountries] = useState([]);
-  const [casesType, setCasesType] = useState("cases");
+  // const [casesType, setCasesType] = useState("cases");
 
 
   useEffect(()=>{
